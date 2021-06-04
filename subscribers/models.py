011d14903eca06cb86b2d0683b41b17dc88e10f1
fquinn03 <fiona_quinn@yahoo.com>
2021-06-04 16:49:31 +0000

    changed subscriber model

--- subscribers/models.py
+++ subscribers/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 
 class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key=True)
-    user_type = models.CharField(max_length=200);
+    TEACHER = 'T'
+    STUDENT = 'S'
+    PARENT = 'P'
+    USER_CHOICES = [
+        (TEACHER, 'Teacher'),
+        (STUDENT, 'Student'),
+        (PARENT, 'Parent'),
+    ]
+    status = models.CharField(
+        max_length=1,
+        choices=USER_CHOICES,
+        default=TEACHER,
+    )
+    subscribed = models.BooleanField(default=False)
 
 

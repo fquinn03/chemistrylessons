29a624ce468524b86900e4d8a8d42483463669ac
fquinn03 <fiona_quinn@yahoo.com>
2021-07-05 14:32:10 +0000

    update to registration process

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -1,12 +1,27 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
 
 class UserForm(UserCreationForm):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = 'Required - 150 characters or fewer. Usernames may contain alphanumeric characters and , _, @, +, . or - '
+        self.fields['email'].help_text = 'Required - enter valid email address'
+        self.fields['password2'].help_text = 'Enter the same password as before for verification'
 
     class Meta:
         model = User
-        fields = ('first_name','last_name', 'username', 'email', 'password1' ,'password2' )
+        fields = ('username', 'email', 'password1' ,'password2' )
+
+class CustomUserForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['status'].label = 'User Type'
+        self.fields['status'].help_text = 'Select user type'
+
+    class Meta:
+        model = CustomUser
+        fields = ['status']

e0c2ddfa53fc7e8b837e933f2ea586b7a3762b29
fquinn03 <fiona_quinn@yahoo.com>
2021-07-10 17:38:14 +0000

    added password reset

--- subscribers/urls.py
+++ subscribers/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('subscribe/', views.subscribe, name='subscribe'),
     path('register/', views.register, name='register'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
